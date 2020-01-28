3ae60fce691e0d6cbce33b50cd23e1dfbc8115ed
myrdstom <nserekopaul@gmail.com>
2020-01-28 16:00:17 +0300

    setup firebase

--- src/components/profile/container/EditProfileView.js
+++ src/components/profile/container/EditProfileView.js
@@ -1,17 +1,148 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Loader from '../../Loader';
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import EditProfile from '../component/EditProfile';
 
 class EditProfileView extends Component {
-    render() {
-        return (
-            <div>
-                Edit Profile
+    constructor() {
+        super();
+        this.state = {
+            avatar: '',
+            firstName: '',
+            lastName: '',
+            bio: '',
+            errors: {},
+        };
+    }
+    // componentDidMount() {
+    //     if (!this.props.auth.isAuthenticated) {
+    //         this.props.history.push('/');
+    //     }
+    // }
+    // componentDidUpdate(prevProps, prevState, snapshot) {
+    //     if (!this.props.auth.isAuthenticated) {
+    //         this.props.history.push('/');
+    //     }
+    // }
 
-            </div>
+    // componentWillReceiveProps(nextProps) {
+    //     if (nextProps.errors) {
+    //         this.setState({ errors: nextProps.errors }, () => {
+    //             console.log(this.state.errors);
+    //         });
+    //     }
+    //
+    //     if (nextProps.profile.profile && nextProps.profile.loading === false) {
+    //         const {
+    //             firstName,
+    //             lastName,
+    //             bio,
+    //             avatar,
+    //         } = nextProps.profile.profile;
+    //         this.setState({
+    //             avatar,
+    //             firstName,
+    //             lastName,
+    //             bio,
+    //         });
+    //     }
+    // }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    mouseClick = () => {
+        window.cloudinary.openUploadWidget(
+            {
+                cloudName: 'dr8lvoqjj',
+                uploadPreset: 'gt2kahpt',
+                cropping: true,
+                folder: 'widgetdocs',
+                sources: [
+                    'local',
+                    'url',
+                    'camera',
+                    'facebook',
+                    'dropbox',
+                    'search',
+                    'instagram',
+                ],
+            },
+            (error, result) => {
+                if (result.event === 'success') {
+                    let newImage = result.info.secure_url;
+                    window.localStorage.setItem('newImage', newImage);
+                    window.localStorage.setItem('image', newImage);
+                    window.location.reload();
+                }
+            },
         );
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const profileData = {
+            avatar:
+                window.localStorage.getItem('newImage') || this.state.avatar,
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            bio: this.state.bio,
+        };
+        const { createProfile, history } = this.props;
+
+        createProfile(profileData, history);
+    };
+
+    render() {
+        const { profiles } = this.props;
+        if (profiles) {
+            const profile = {
+                id: profiles[0].id,
+                firstName: profiles[0]['firstName'],
+                lastName: profiles[0]['lastName'],
+                email: profiles[0]['email'],
+                avatar:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTSVWuCurkB8xwYNcygqZlUPJdUvmfCoOiyQZk1L74c6cX-6Boq',
+                phone: profiles[0]['Phone Number'],
+                address: profiles[0]['Address'],
+                dateOfBirth: '13-04-2020',
+            };
+
+            return (
+                <div>
+                    <EditProfile
+                        avatar={profile.avatar}
+                        firstName={profile.firstName}
+                        lastName={profile.lastName}
+                        onMouseClick={this.mouseClick}
+                        onChange={this.handleChange}
+                        onSubmit={this.handleSubmit}
+                    />
+                </div>
+            );
+        } else {
+            return (
+                <h1>
+                    <Loader />
+                </h1>
+            );
+        }
     }
 }
 
-EditProfileView.propTypes = {};
+EditProfileView.propTypes = {
+    firestore: PropTypes.object.isRequired,
+    users: PropTypes.array,
+};
 
-export default EditProfileView;
+export default compose(
+    firestoreConnect([{ collection: 'profiles' }]),
+    connect((state, props) => ({
+        profiles: state.firestore.ordered.profiles,
+    })),
+)(EditProfileView);
---
+++ src/firebase/store.js
@@ -0,0 +1,52 @@
+import { createStore, combineReducers, compose } from 'redux';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+//Reducers
+//@todo
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyD9I68JrMjGafhQByyihSWdnkY5ThfRh0k',
+    authDomain: 'jetcake-94931.firebaseapp.com',
+    databaseURL: 'https://jetcake-94931.firebaseio.com',
+    projectId: 'jetcake-94931',
+    storageBucket: 'jetcake-94931.appspot.com',
+    messagingSenderId: '875949350235',
+    appId: '1:875949350235:web:a00e041d20b8d474498e7e',
+    measurementId: 'G-WE2MJ42X8F',
+};
+
+// react-redux-firebase-config
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true,
+};
+
+// Init firebase instance
+firebase.initializeApp(firebaseConfig);
+// Init firestore
+export const firestore = firebase.firestore();
+
+const createStoreWithFirebase = compose(
+    reactReduxFirebase(firebase, rrfConfig),
+    reduxFirestore(firebase),
+)(createStore);
+
+const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+    firestore: firestoreReducer
+});
+
+// Initial State
+const initialState={};
+
+//Create store
+const store = createStoreWithFirebase(rootReducer, initialState, compose(
+    reactReduxFirebase(firebase),
+    composeWithDevTools()
+));
+
+export default store;
